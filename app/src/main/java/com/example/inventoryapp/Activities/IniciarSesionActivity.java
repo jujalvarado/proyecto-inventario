b69e868d77ce19328aebb04d3b0507037fb6068e
Juan Alvarado <jujalvarado@est.istg.edu.ec>
2023-03-13 02:39:15 -0500

    pequeño avance en el proceso de login y la base de datos - juan

--- app/src/main/java/com/example/inventoryapp/Activities/IniciarSesionActivity.java
+++ app/src/main/java/com/example/inventoryapp/Activities/IniciarSesionActivity.java
@@ -2,17 +2,35 @@ package com.example.inventoryapp.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.provider.SyncStateContract.Constants;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.example.inventoryapp.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class IniciarSesionActivity extends AppCompatActivity {
 
+    String url_iniciar_sesion = "https://inventoryappweb.000webhostapp.com/iniciar_sesion.php";
+
+
+    private EditText editTextemail, editTextcontrasena;
     private Button btnIniciarSesion;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +39,22 @@ public class IniciarSesionActivity extends AppCompatActivity {
 
         btnIniciarSesion = findViewById(R.id.ibuttonIniciarSesion);
 
+        editTextemail = findViewById(R.id.ieditTextEmail);
+        editTextcontrasena = findViewById(R.id.ieditTextContrasena);
+
+
+        // barra de proceso
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Iniciando Sesion...");
+        progressDialog.setCancelable(false);
+
+
         btnIniciarSesion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                iniciarsesion();
+
                 Toast.makeText(IniciarSesionActivity.this, "Sesión Iniciada con éxito", Toast.LENGTH_SHORT).show();
                 Intent intent= new Intent(IniciarSesionActivity.this,MainActivity.class);
 
@@ -32,6 +63,73 @@ public class IniciarSesionActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void iniciarsesion(){
+
+        String correo = editTextemail.getText().toString().trim();
+        String contrasena = editTextcontrasena.getText().toString().trim();
+
+
+        // valida el campo email
+        if(TextUtils.isEmpty(correo)){
+            editTextemail.setError("Ingrese su correo");
+            editTextemail.requestFocus();
+            return;
+        }
+
+        // valida el campo email
+        if(!Patterns.EMAIL_ADDRESS.matcher(correo).matches()){
+            editTextemail.setError("Ingrese un correo válido");
+            editTextemail.requestFocus();
+            return;
+        }
+
+        // valida el campo email
+        if(TextUtils.isEmpty(contrasena)){
+            editTextcontrasena.setError("Ingrese su contraseña");
+            editTextcontrasena.requestFocus();
+            return;
+        }
+
+        // se muestra la barra de progreso
+        progressDialog.show();
+
+
+        StringRequest stringRequet = new StringRequest(Request.Method.POST, url_iniciar_sesion, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                progressDialog.dismiss();
+
+                try{
+                    JSONObject jsonObject = new JSONObject(response);
+                    if(!jsonObject.getBoolean("error")){
+
+                        Toast.makeText(IniciarSesionActivity.this, "Inicio de sesión inválido", Toast.LENGTH_SHORT).show();
+
+
+//                        SharedPreferences sharedPreferences = getSharedPreferences(Constants.SHARED_PREFS , MODE_PRIVATE)
+
+                    }
+
+                }catch(JSONException e){
+
+
+                }
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }
+
+        ){
+
+
+        };
 
 
     }
--- app/src/main/java/com/example/inventoryapp/Activities/RegistrarseActivity.java
+++ app/src/main/java/com/example/inventoryapp/Activities/RegistrarseActivity.java
@@ -2,33 +2,224 @@ package com.example.inventoryapp.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.example.inventoryapp.R;
+import com.example.inventoryapp.VolleySingleton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegistrarseActivity extends AppCompatActivity {
 
+    String url_registrar = "https://inventoryappweb.000webhostapp.com/usuario_insertar.php";
 
     private Button btnRegistrarse;
+    private EditText editTextnombreusuario, editTextemailusuario, editTextcontrasena,
+                     editTextnombretienda, editTextdirecciontienda,editTexttelefonotienda ;
+    private ProgressDialog progressDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrarse);
 
+        editTextnombreusuario = findViewById(R.id.reditTextNombres);
+        editTextemailusuario = findViewById(R.id.reditTextEmail);
+        editTextcontrasena = findViewById(R.id.reditTextContrasena);
+
+        editTextnombretienda = findViewById(R.id.reditTextNombret);
+        editTextdirecciontienda = findViewById(R.id.reditTextdireccion);
+        editTexttelefonotienda = findViewById(R.id.reditTexttelefono);
+
         btnRegistrarse = findViewById(R.id.rbuttonRegistrarse);
+
+        // barra de proceso
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Registrando Usuario...");
+        progressDialog.setCancelable(false);
+
+
+        // boton registarse llama al metodo para registrar usuario,
+        // muestra un mensaje y cierra la activity y retorna al inicio
 
         btnRegistrarse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(RegistrarseActivity.this, "Registro exitoso", Toast.LENGTH_SHORT).show();
+                registrarUsuario();
                 finish();
             }
         });
 
     }
 
+    // metodo para registrar el usuario y asu vez la tienda ingresada
+    private void registrarUsuario(){
+
+        String nombre = editTextnombreusuario.getText().toString();
+        String correo = editTextemailusuario.getText().toString().trim();
+        String contrasena = editTextcontrasena.getText().toString().trim();
+
+        String nombretienda = editTextnombretienda.getText().toString();
+        String direcciontienda = editTextdirecciontienda.getText().toString();
+        String telefono = editTexttelefonotienda.getText().toString().trim();
+
+
+        // valida el campo nombre
+        if(TextUtils.isEmpty(nombre)){
+            editTextnombreusuario.setError("Ingrese sus nombres y apellidos");
+            editTextnombreusuario.requestFocus();
+            return;
+        }
+
+        // valida el campo email
+        if(!Patterns.EMAIL_ADDRESS.matcher(correo).matches()){
+            editTextemailusuario.setError("Ingrese un correo válido");
+            editTextemailusuario.requestFocus();
+            return;
+        }
+
+        // valida el campo contrasena y que tenga mas de 6 caracteres
+        if(TextUtils.isEmpty(contrasena)){
+            editTextcontrasena.setError("Ingrese su contraseña");
+            editTextcontrasena.requestFocus();
+            return;
+        }
+
+        if(contrasena.length() < 6){
+            editTextcontrasena.setError("Su contraseña debe tener mas 6 caracteres");
+            editTextcontrasena.requestFocus();
+            return;
+        }
+
+        //valida el campo nombre de la tienda
+        if(TextUtils.isEmpty(nombretienda)){
+            editTextnombretienda.setError("Ingrese el nombre de su tienda/negocio");
+            editTextnombretienda.requestFocus();
+            return;
+        }
+
+        //valida el campo direccion de la tienda
+        if(TextUtils.isEmpty(direcciontienda)){
+            editTextdirecciontienda.setError("Ingrese la dirección de su tienda/negocio");
+            editTextdirecciontienda.requestFocus();
+            return;
+        }
+
+        // valida el campo telefono de la tienda y que tenga 10 digitos numericos
+        if(TextUtils.isEmpty(telefono)){
+            editTexttelefonotienda.setError("Ingrese el numero de telefono/celular");
+            editTexttelefonotienda.requestFocus();
+            return;
+        }
+
+        String regex = "\\d{10}" ;
+
+        if(telefono.matches(regex)){
+            editTexttelefonotienda.setError("Debe tener al menos 10 digitos numericos");
+            editTexttelefonotienda.requestFocus();
+            return;
+        }
+
+        // se muestra la barra de progreso
+        progressDialog.show();
+
+
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST,url_registrar, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                progressDialog.dismiss();
+
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+
+
+                    //
+                    if (!jsonObject.getBoolean("error")) {
+
+                        Toast.makeText(RegistrarseActivity.this,
+                                jsonObject.getString("message"),
+                                Toast.LENGTH_SHORT)
+                                .show();
+
+                        Intent intent = new Intent(RegistrarseActivity.this, StartActivity.class);
+                        startActivity(intent);
+                        finish();
+
+                    } else {
+
+                        Toast.makeText(RegistrarseActivity.this,
+                                jsonObject.getString("message"),
+                                Toast.LENGTH_SHORT)
+                                .show();
+                    }
+
+                } catch (JSONException e) {
+
+                    e.printStackTrace();
+
+                    Toast.makeText(RegistrarseActivity.this,
+                            "Error al registrar al usuario: " + e.getMessage(),
+                            Toast.LENGTH_SHORT)
+                            .show();
+                }
+            }
+        },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                        progressDialog.dismiss();
+
+                        Toast.makeText(RegistrarseActivity.this,
+                                "Error al registrar al usuario: "+ error.getMessage(),
+                                Toast.LENGTH_SHORT)
+                                .show();
+                    }
+                }
+
+        ) {
+            @Override
+
+            protected Map<String,String> getParams() throws AuthFailureError {
+
+                Map<String,String> params = new HashMap<>();
+
+                params.put("nombre_usuario",nombre);
+                params.put("correo",correo);
+                params.put("contrasena",contrasena);
+
+                params.put("nombre_tienda",nombretienda);
+                params.put("direccion",direcciontienda);
+                params.put("telefono",telefono);
+
+                return params;
+
+            }
+        };
+
+
+        VolleySingleton.getInstance(this).addToRequestQueue(stringRequest);
+    }
 }
